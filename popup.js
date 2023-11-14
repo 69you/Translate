6ed520a2ee5389ed097af1f240b289118ad59657
侑介 <spark20060609@gmail.com>
2023-11-14 22:05:32 +0800

    combine it into a func

--- popup.js
+++ popup.js
@@ -2,29 +2,22 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('translateButton').addEventListener('click', function () {
         const textToTranslate = document.getElementById('inputText').value;
         if (textToTranslate) {
-            translateText(textToTranslate, function (translatedText) {
-                copyToClipboard(translatedText);
-            });
+            fetch(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=zh-TW&tl=zh-CN&dt=t&q=${encodeURIComponent(textToTranslate)}`)
+            .then(response => response.json())
+            .then(data => {
+                const text = data[0][0][0];
+                navigator.clipboard.writeText(text)
+                .then(() => {
+                    alert(`Success Copied:\n${text}`);
+                })
+                .catch(e => {
+                    alert('Failed Copied', e);
+                });
+            })
+            .catch(e => alert('Err while Translate', e));
+        }
+        else {
+            alert('Please input a text');
         }
     });
 });
-
-function translateText(text, callback) {
-    fetch(`https://translate.googleapis.com/translate_a/single?client=gtx&sl=zh-TW&tl=zh-CN&dt=t&q=${encodeURIComponent(text)}`)
-        .then(response => response.json())
-        .then(data => {
-            const translatedText = data[0][0][0];
-            callback(translatedText);
-        })
-        .catch(e => alert('Err while Translate', e));
-}
-
-function copyToClipboard(text) {
-    navigator.clipboard.writeText(text)
-        .then(() => {
-            alert(`Success Copied:\n${text}`);
-        })
-        .catch(e => {
-            alert('Failed Copied', e);
-        });
-}
